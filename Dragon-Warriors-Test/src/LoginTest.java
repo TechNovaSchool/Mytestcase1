5e6319641f6affe486722eb5876cba831c9da1f8
andrewnova <tech.easy1@outlook.com>
2021-06-17 18:17:52 -0500

    fix the bugs

--- Dragon-Warriors-Test/src/LoginTest.java
+++ Dragon-Warriors-Test/src/LoginTest.java
@@ -2,6 +2,5 @@ public class LoginTest {
     public static void main(String[] args) {
         String username = "bondjames";
         String password = "12345pass";
-        System.out.printf("Login is successful");
     }
 }
