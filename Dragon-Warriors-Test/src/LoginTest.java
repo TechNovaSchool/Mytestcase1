6c500347b70f0a682a70fc94b37c9f3996e4c19b
andrewnova <tech.easy1@outlook.com>
2021-06-17 18:13:43 -0500

    tyet

---
+++ Dragon-Warriors-Test/src/LoginTest.java
@@ -0,0 +1,8 @@
+public class LoginTest {
+    public static void main(String[] args) {
+        String username = "userbond";
+        String password = "1234pass";
+        System.out.printf("Login is succesful");
+
+    }
+}
